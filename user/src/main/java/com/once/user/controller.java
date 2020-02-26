78a93c918a3e8d33a76a1943eadf92bd460cc4ed
pacomonle <franciscomonleonleal@gmail.com>
2020-02-26 16:34:08 +0100

    class usuario java

--- user/src/main/java/com/once/user/controller.java
+++ user/src/main/java/com/once/user/controller.java
@@ -1,6 +1,12 @@
 package com.once.user;
 
+import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
